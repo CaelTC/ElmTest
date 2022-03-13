27ef5534946a00f2df80234701efbbafb77cfeb4
CaelTC <caeltrudeaucauchon@gmail.com>
2022-03-13 14:12:01 -0400

    Ajout de bouton pour vrai...

---
+++ Button/src/Main.elm
@@ -0,0 +1,69 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, text, div, h1, img, button)
+import Html.Attributes exposing (src)
+import Html.Events exposing (onClick)
+
+
+---- MODEL ----
+
+
+type alias Model = 
+    {number : Int
+    |listNumber : List Int}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {number = 0, listNumber = []}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = Increment
+    | Decrement
+    | SaveToList
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increment ->
+            ( {model | model.number + 1} , Cmd.none)
+        Decrement ->
+            ( {model | model.number - 1} , Cmd.none)
+        SaveToList ->
+            ( model.number :: model.listNumber, Cmd.none)
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Increment] [text "+"]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Decrement] [text "-"]
+        , div [] [ text (List.map (model String.fromInt))]
+        , button [ onClick SaveToList][text "Save"]]
+    
+        
+
+
+
+---- PROGRAM ----
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = always Sub.none
+        }
